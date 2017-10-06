6a5158579057029b64ad0b025324f06c804ee084
Zahra Khan <zarssskhan@gmail.com>
2017-10-06 06:29:10 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,6 +2,7 @@ def turn_count(board)
   #keep track of the numer of turns
   # start at 0
   turn = 0
+  # loop over elements of the board
   board.each do |player|
     if player == "X" || player == "O"
       turn += 1
