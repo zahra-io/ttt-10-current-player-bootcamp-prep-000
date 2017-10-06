42d7c7b513e511bdd723760cb0b47627b8644f9a
Zahra Khan <zarssskhan@gmail.com>
2017-10-06 06:23:51 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   turn = 0
   board.each do |player|
-    if player == "X" || player == ")"
-
-    counter += 1
+    if player == "X" || player == "O"
+    turn += 1
+  end
 end
